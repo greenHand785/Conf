75eaa974716830831d44d072782e97cbca397c2f
greenHand785 <2311632028@qq.com>
2022-03-06 08:53:38 +0800

    新增，存盘协议，武器配置，聊天通讯协议

--- proto/gen_cfg.bat
+++ proto/gen_cfg.bat
@@ -1,1 +1,3 @@
-# 用于生成客户端配置
+:: 用于生成客户端配置
+
+:: D:\MyProjects\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\MyProjects\FrameSyncFPSGame\develop\conf\proto\mod_proto\character.proto --output D:\MyProjects\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\CharacterMod.cs
--- proto/mod_proto.bat
+++ proto/mod_proto.bat
@@ -1,10 +1,13 @@
 :: 用于生成客户端协议文件的
 
-D:\MyProjects\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\MyProjects\FrameSyncFPSGame\develop\conf\proto\mod_proto\mod.proto --output D:\MyProjects\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\Mod.cs
-D:\MyProjects\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\MyProjects\FrameSyncFPSGame\develop\conf\proto\mod_proto\login_mod.proto --output D:\MyProjects\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\LoginMod.cs
-D:\MyProjects\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\MyProjects\FrameSyncFPSGame\develop\conf\proto\mod_proto\chat_mod.proto --output D:\MyProjects\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\ChatMod.cs
+D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\Mod.cs
+D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\login_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\LoginMod.cs
+D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\chat_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\ChatMod.cs
+D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\hall_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\HallMod.cs
+
+
 
 :: 存盘协议
-D:\MyProjects\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\MyProjects\FrameSyncFPSGame\develop\conf\proto\mod_proto\character.proto --output D:\MyProjects\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\CharacterMod.cs
+D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\character.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\CharacterMod.cs
 
 pause
