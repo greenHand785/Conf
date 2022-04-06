b5aa7c0125f25160daa8d3df6094fdab937f4d16
greenHand785 <2311632028@qq.com>
2022-04-06 18:26:07 +0800

    增加hall各种协议

--- proto/mod_proto.bat
+++ proto/mod_proto.bat
@@ -4,6 +4,7 @@ D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master
 D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\login_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\LoginMod.cs
 D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\chat_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\ChatMod.cs
 D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\hall_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\HallMod.cs
+D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\protobuf-master\CodeGenerator\bin\Debug\net472\CodeGenerator.exe D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\conf\proto\mod_proto\enum_mod.proto --output D:\YgyProject\ygy\ygyProject\bishe\FrameSyncFPSGame\develop\MultiFpsGame\Assets\Scripts\PBMsg\EnumMod.cs
 
 
 
